dea640fecd3671f3083135dc6289e1dbb352d81f
CheesyHero <0urher0nik0@gmail.com>
2026-06-30 17:30:32 +0000

    updated formatting

--- handlers/promotional.go
+++ handlers/promotional.go
@@ -8,8 +8,8 @@ import (
 
 func AdminPromotionalsPage(c *fiber.Ctx) error {
 	page := fiber.Map{
-		"Page":  structs.Page{PageName: "Promotionals"},
-		"Tabs":  utils.CurrentTabs(),
+		"Page":  structs.Page{PageName: "Promotionals", 
+		Tabs: utils.CurrentTabs(), Classes: utils.Classes},
 	}
-	return c.Render("admin_promotionals", page)
+	return c.Render("adminPage", page)
 }
